b9b46c4912c7e433e0251de44aa3ceb18af15659
Andrew Dennis <adennis@groupon.com>
2012-08-07 23:46:21 -0500

    adds booking index page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,4 +3,11 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
+
+  def index
+    @stat_array = Booking.find_all_by_name("Google Inc.").map { |order| order.metric_1 }
+  
+    @max = @stat_array.max
+    @min = @stat_array.min
+  end
 end
