ea90d8d3c3e6daed458332741d2a3f421a93ccde
Andrew Dennis <adennis@groupon.com>
2012-08-16 00:39:32 -0500

    simplifies booking model / finishes variable change from stat_array

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,26 +1,25 @@
 class Booking < ActiveRecord::Base
-  attr_accessible :metric_1, :name, :booking_history_id
+  attr_accessible :metric_1, :name, :booking_snapshot_id
 
   has_one :cancellation
   belongs_to :booking_history
 
   validates :name, :presence => true
   validates :metric_1, :presence => true, :numericality => true
-  validates :booking_history_id, :presence => true, :numericality => true
+  validates :booking_snapshot_id, :presence => true, :numericality => true
 
   def update_bookings
-    dummy_symbols = ['GOOG', 'LNKD', 'ZNGA']
-    new_history = BookingHistory.create
-    dummy_symbols.each do |symbol|
-      dummy_data = YahooStock::Quote.new(:stock_symbols => symbol)
-      dummy_data.use_all_parameters
-      dummy_data_hash = dummy_data.results(:to_hash).output.first
+    symbol = 'GOOG'
+    new_history = BookingSnapshot.create
+    test_data = YahooStock::Quote.new(:stock_symbols => symbol)
+    test_data.use_all_parameters
+    test_data_hash = test_data.results(:to_hash).output.first
 
-      b = Booking.new
-      b[:name] = dummy_data_hash[:name]
-      b[:metric_1] = dummy_data_hash[:last_trade_price_only]
-      b[:booking_history_id] = new_history.id
-      b.save!
+    b = Booking.new
+    b[:name] = test_data_hash[:name]
+    b[:metric_1] = test_data_hash[:last_trade_price_only]
+    b[:booking_snapshot_id] = new_history.id
+    b.save!
     end
   end
 end
