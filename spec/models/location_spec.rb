aa2c6ef34059551c5222bbdbb70359358c110cf4
Andrew Dennis <adennis@groupon.com>
2012-09-06 15:07:37 -0500

    adds locations spec

---
+++ spec/models/location_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Location do
+  # describe "set_location" do
+  #   it "adds city, state, country, lat, and lng to database" do
+  #     new_location = Location.new
+  #     new_location.set_location("62.87.0.0")
+  #     new_location.city.should == "Barcelona"
+  #     new_location.state.should == nil
+  #     new_location.country.should == "ES"
+  #     new_location.latitude.should == 41.3833
+  #     new_location.longitude.should == 2.15
+  #   end
+  # end
+end
